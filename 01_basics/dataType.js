6268a93f20d8ec372d75eea5cd1ba9c90586af32
aprakash125 <ananyaprakash29@gmail.com>
2024-11-15 05:35:57 +0000

    datatypes

---
+++ 01_basics/dataType.js
@@ -0,0 +1,11 @@
+"use strict";//to new new version of java script
+
+//number - 2 to the power 53
+//string=>""
+// bigint+=> use for big numbers
+//boolean=> true false
+//null=> standlone value type and type of null is object
+//undefined=> value intialtised but not assigened yet its same as undifined
+//symbol=> to find unique ness
+
+//obect
---
+++ 01_basics/variablesname.js
@@ -0,0 +1,12 @@
+const account=22187
+let accountEmail="ananyaprakash4@gmail.com"
+var accountPasswrd="lipika"
+accountCity="jaipur"
+accountPasswrd="6528"
+accountCity="patna"
+accountEmail="priya"
+var accountState;
+console.table([account, accountEmail, accountPasswrd,accountCity,accountState]);
+console.log(accountEmail);
+//try not to use the var keyword 
+//because its block scope issue
