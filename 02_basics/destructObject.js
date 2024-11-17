95f6465f242d2875f6115e876b105d9c74b54725
aprakash125 <ananyaprakash29@gmail.com>
2024-11-17 15:20:49 +0000

    destruct

---
+++ 02_basics/destructObject.js
@@ -0,0 +1,9 @@
+const couurse= {
+    couseNmae:"javascript",
+    prize:999,
+    teacher:"ananya"
+}
+const {couseNmae}=couurse
+console.log(couseNmae)
+const {couseNmae:name}=couurse
+console.log(name)
